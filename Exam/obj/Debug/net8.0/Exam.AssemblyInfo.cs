f4ab14407f1dfff3e7a0da6e6f7b207fbc267f09
Льоня Крайнічук <lonakrajnicuk@gmail.com>
2026-05-12 20:23:53 +0300

    Fix: correct csproj name and bypass vs lock

--- Exam/obj/Debug/net8.0/Exam.AssemblyInfo.cs
+++ Exam/obj/Debug/net8.0/Exam.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3358fee673a8eb460ac5d4213bb9e3af362bc0e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
